1d48e365f223aea737d3b4c5dc0aa00f5c55e57a
fl6145 <fl_6145@163.com>
2021-06-09 12:58:08 +0800

    整合fastjson

--- demo-fastjson/src/main/java/tk/fulsun/demo/FastApplication.java
+++ demo-fastjson/src/main/java/tk/fulsun/demo/FastApplication.java
@@ -1,5 +1,7 @@
 package tk.fulsun.demo;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,5 +14,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FastApplication {
   public static void main(String[] args) {
     SpringApplication.run(FastApplication.class, args);
+  }
+
+  public static String obj2json(Object obj) {
+    String json = "";
+    try {
+      json =
+          JSONObject.toJSONString(
+              obj,
+              // collection 空值输出 []
+              SerializerFeature.WriteMapNullValue,
+              // 字符串类型空值输出空字符串""
+              SerializerFeature.WriteNullStringAsEmpty,
+              // 数值类型的空值输出为0
+              SerializerFeature.WriteNullNumberAsZero,
+              // 布尔类型为null输出false
+              SerializerFeature.WriteNullBooleanAsFalse,
+              SerializerFeature.DisableCircularReferenceDetect);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "";
+    }
+    return json;
   }
 }
